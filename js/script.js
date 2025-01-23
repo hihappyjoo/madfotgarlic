f26b7b127d2e62ad9df3814b6d472974f8da6bb5
moodang teacher <yssmi@naver.com>
2025-01-23 17:20:58 +0900

    정리 작업

--- js/script.js
+++ js/script.js
@@ -1,38 +1,34 @@
-const promotionSlider = new Swiper(".promotion-slider", {
+const promotionSlider = new Swiper('.promotion-slider', {
     loop: true,
     autoplay: true,
     speed: 1000,
 
     // If we need pagination
     pagination: {
-        el: ".swiper-pagination",
+        el: '.swiper-pagination',
         clickable: true,
     },
 
     // Navigation arrows
     navigation: {
-        nextEl: ".btn-next",
-        prevEl: ".btn-prev",
+        nextEl: '.btn-next',
+        prevEl: '.btn-prev',
     },
 });
 
-const lunchSetSlider = new Swiper(".lunch-set-slider", {
+const lunchSetSlider = new Swiper('.lunch-set-slider', {
     // Optional parameters
     loop: true,
     // slidesPerView: "auto" /* wrapper 가로길이 / 내가 설정한 slide길이 갯수만큼 보임 */,
 
     // slidesPerView: auto 주면 transform: scale() 했을때 밀림
     // => slidesPerView: 4 수정 -> .lunch-set-slider 전체 크기를 디자인과 맞춤 width: 1988px; /* (467+40 * 4)-40 */
-    slidesPerView: 1.2,
+    slidesPerView: 2,
     spaceBetween: 20,
 
     // 반응형 분기점
     breakpoints: {
         820: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
-        1440: {
             slidesPerView: 4,
             spaceBetween: 40,
         },
@@ -40,11 +36,11 @@ const lunchSetSlider = new Swiper(".lunch-set-slider", {
 
     // Navigation arrows
     navigation: {
-        nextEl: ".btn-next",
+        nextEl: '.btn-next',
     },
 });
 
-const followSlider = new Swiper(".follow-slider", {
+const followSlider = new Swiper('.follow-slider', {
     // Optional parameters
     loop: true,
     slidesPerView: 1.7,
@@ -66,7 +62,7 @@ const followSlider = new Swiper(".follow-slider", {
 
     // If we need pagination
     pagination: {
-        el: ".swiper-pagination",
+        el: '.swiper-pagination',
         dynamicBullets: true,
         dynamicMainBullets: 1,
     },
