450bf9f8aee2a9531d454df88ec393f88e769a7f
hijoo <fluffybulb@gmail.com>
2025-02-03 20:42:07 +0900

    lunch additinal bp .4

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+const subMenuSwiper = new Swiper(".sub-menu-swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: false,
+
+    slidesPerView: "auto",
+    spaceBetween: 31,
+
+    // 질문:  이거 1440에서는 전체가 보이는데 뒤에 빈공간이 남아여
+});
