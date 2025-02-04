8737f715b969f9b02485623b3cd988eb31072501
moodang teacher <yssmi@naver.com>
2025-02-04 14:23:20 +0900

    1440이상에서 슬라이드 터치 및 드래그 비활성화

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,18 @@
-const subMenuSwiper = new Swiper(".sub-menu-swiper", {
+const subMenuSwiper = new Swiper('.sub-menu-swiper', {
     // Optional parameters
-    direction: "horizontal",
+    direction: 'horizontal',
     loop: false,
 
-    slidesPerView: "auto",
+    slidesPerView: 'auto',
     spaceBetween: 31,
 
     // 질문:  이거 1440에서는 전체가 보이는데 뒤에 빈공간이 남아여
+
+    breakpoints: {
+        1440: {
+            noSwiping: true,
+            noSwipingClass: 'sub-menu-swiper',
+            allowTouchMove: false,
+        },
+    },
 });
